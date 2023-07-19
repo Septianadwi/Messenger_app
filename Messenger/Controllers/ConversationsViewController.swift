282a1447ab12ba93b3e0ee3ea0abb99e78abca14
ana.sdp <107457070+Septianadwi@users.noreply.github.com>
2023-07-20 01:07:32 +0700

    Login/Register

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,19 +1,33 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
-//  Created by Mac Pro on 16/07/23.
+//  Created by Mac Pro on 19/07/23.
 //
 
 import UIKit
 
 class ConversationsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
+        
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
 }
-
